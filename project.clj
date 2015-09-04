93e954fdbc9444a337bfe68c622d1f9b3cac3b54
Avi Flax <avi@aviflax.com>
2015-09-03 23:33:39 -0400

    Remove inherited EPL

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject omnia-poc "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :license {:name "TBD"
+            :url "TBD"}
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [com.datomic/datomic-free "0.9.5206"] ; not available on clojars or maven central; must be added to local maven repository cache thingy
                  [clucy "0.4.0"]
