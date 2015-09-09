b3bf0f98d868de6aedb6d60e4e10ed21a70f7773
Avi Flax <avi@aviflax.com>
2015-09-08 21:08:28 -0400

    Clean up dropbox code; make it more consistent with GDrive

--- src/omnia_poc/dropbox.clj
+++ src/omnia_poc/dropbox.clj
@@ -34,20 +34,22 @@
                     :omnia-source-id (lower-case (.path file))
                     ; lower-case to work around a possible bug in clucy
                     :omnia-source (lower-case (:name source)))]
-    (if (.endsWith (.path file) ".txt")                     ; TODO: make this much more sophisticated!
+    (if (or (.endsWith (.path file) ".txt")                 ; TODO: make this much more sophisticated!
+            (.endsWith (.path file) ".md"))
         (assoc f :text (get-file-content (.path file) client))
         f)))
 
 (defn process-delta-entry! [client source entry]
-  (if (nil? (.metadata entry))
-      (lucene/delete-file {:omnia-source (lower-case (:name source))
-                           :omnia-source-id (lower-case (.lcPath entry))})
-      (when (.isFile (.metadata entry))                     ; TODO: handle directories?
-        (println (-> (.metadata entry) .path))
-        (lucene/add-or-update-file (dropbox-file->omnia-file-with-text client source (.metadata entry)))
-        (Thread/sleep 10))))
+  (if-let [md (.metadata entry)]
+    (when (.isFile md)                                      ; TODO: handle directories?
+      (println (.path md))
+      (-> (dropbox-file->omnia-file-with-text client source md)
+          lucene/add-or-update-file)
+      (Thread/sleep 10))
+    (lucene/delete-file {:omnia-source (lower-case (:name source))
+                         :omnia-source-id (lower-case (.lcPath entry))})))
 
-(defn synchronize [{:keys [sync-cursor] :as source}]
+(defn synchronize! [{:keys [sync-cursor] :as source}]
   (let [client (get-client source)]
     (loop [cursor sync-cursor]
       (let [delta (.getDelta client cursor)]
