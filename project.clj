bcb12d3796ccef386dbeb052bfc8cac2e3fb3fa4
Avi Flax <avi@aviflax.com>
2015-09-09 21:44:45 -0400

    Tweaks to lein deps

--- project.clj
+++ project.clj
@@ -6,12 +6,13 @@
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [com.datomic/datomic-free "0.9.5206"]      ; not available on clojars or maven central; must be added to local maven repository cache thingy
                  [clucy "0.4.0"]
+                 [com.dropbox.core/dropbox-core-sdk "1.7" :exclusions [com.fasterxml.jackson.core/jackson-core]]
                  [clj-http "2.0.0"]
                  [cheshire "5.5.0"]
-                 [com.dropbox.core/dropbox-core-sdk "[1.7,1.8)"]
 
                  ; these deps are not used directly but I’m specifying them to address conflicts
-                 [commons-codec "1.10"]]
+                 [commons-codec "1.10"]
+                 [joda-time "2.8.2"]]
   :main omnia-poc.core
   :target-path "target/%s"
   :profiles {:dev {:dependencies [[cursive/datomic-stubs "0.9.5153" :scope "provided"]]}
