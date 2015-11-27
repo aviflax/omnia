be39c820316f7abb4606985cfa65fb17044bbf5e
Avi Flax <avi@aviflax.com>
2015-11-27 13:05:58 -0500

    Dropbox Syncing a little faster, smarter, cleaner

--- src/omnia/dropbox.clj
+++ src/omnia/dropbox.clj
@@ -1,7 +1,7 @@
 (ns omnia.dropbox
   (:require [omnia [db :as db]
              [index :as lucene]]
-            [clojure.string :refer [lower-case]])
+            [clojure.string :refer [lower-case split]])
   (:import [com.dropbox.core DbxAppInfo DbxRequestConfig DbxWebAuthNoRedirect DbxClient]
            java.util.Locale
            java.io.ByteArrayOutputStream))
@@ -27,6 +27,16 @@
     (.getFile client path nil stream)
     (str stream)))
 
+(defn should-get-full-text? [file]
+  (or (.endsWith (.path file) ".txt")                 ; TODO: make this much more sophisticated!
+      (.endsWith (.path file) ".md")))
+
+(defn should-index? [metadata-entry]
+  (and (.isFile metadata-entry)
+       (not (some #(.startsWith % ".")
+                  (split (.path metadata-entry) #"/")))
+       (should-get-full-text? metadata-entry))) ;; TEMP TEMP Just to speed up full-account indexing
+
 (defn dropbox-file->omnia-file-with-text
   [client account file]
   (let [f (hash-map :name (.name file)
@@ -35,32 +45,34 @@
                     :omnia-file-id (lower-case (.path file)) ; lower-case to work around a possible bug in clucy
                     :omnia-account-id (:id account)
                     :omnia-account-type-name (-> account :type :name))]
-    (if (or (.endsWith (.path file) ".txt")                 ; TODO: make this much more sophisticated!
-            (.endsWith (.path file) ".md"))
+    (if (should-get-full-text? file)
         (assoc f :text (get-file-content (.path file) client))
         f)))
 
 (defn process-delta-entry! [client account entry]
   (if-let [md (.metadata entry)]
-    ; TODO: handle directories? I think not — Omnia is about Documents.
-    ; TODO: skip files wherein any path segment stars with . — e.g. .svn, .git.
-    (when (.isFile md)
-      (println (.path md))
-      (-> (dropbox-file->omnia-file-with-text client account md)
-          lucene/add-or-update-file)
-      (Thread/sleep 5))
-    (lucene/delete-file {:omnia-account-id (:id account)
+    (if (should-index? md)
+        (do
+          (println "indexing" (.path md))
+          (-> (dropbox-file->omnia-file-with-text client account md)
+              lucene/add-or-update-file))
+        (println "skipping" (.path md)))
+    (lucene/delete-file {;;:omnia-account-id (:id account)
                          :omnia-file-id (lower-case (.lcPath entry))})))
 
 (defn synchronize! [{:keys [sync-cursor] :as account}]
   (let [client (get-client account)]
     (loop [cursor sync-cursor]
-      (let [delta (.getDelta client cursor)]
+      (let [;delta (.getDeltaWithPathPrefix client cursor "/Articles")
+            delta (.getDelta client cursor)
+            ]
         (run! (partial process-delta-entry! client account)
               (.entries delta))
 
         ; update account cursor in DB
         (db/update-account account :sync-cursor (.cursor delta))
+
+        (Thread/sleep 5)
 
         ; get more
         (when (.hasMore delta)
