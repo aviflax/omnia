e7c50fcc1a554154131269c24159a350cd6a602c
Avi Flax <avi@aviflax.com>
2016-02-14 21:53:38 -0500

    Index full text of epub files

--- src/omnia/extraction.clj
+++ src/omnia/extraction.clj
@@ -3,7 +3,18 @@
 (defn can-parse?
   "I think this really should be added to pantomime — what I really want to know is what mime types it’s capable
    of extracting text from. So that “knowledge” should be localized, contained within Pantomime alongside the
-   functionality. But I don’t have time for that right now."
+   functionality. But I don’t have time for that right now.
+
+   I was just poking around in the Tika docs and actually it looks like there might be a way to
+   extract this information from Tika programmatically:
+
+   * The Tika interface [Parser](https://tika.apache.org/1.4/api/org/apache/tika/parser/Parser.html)
+     has the method `getSupportedTypes` that returns `Set<MediaType>`.
+
+   * The Tika class [AutoDetectParser](https://tika.apache.org/1.4/api/org/apache/tika/parser/AutoDetectParser.html)
+     implements Parser
+
+   * So instantiating AutoDetectParser and then calling `getSupportedTypes` might just be the ticket!"
   [mime-type]
   (true?
     (some #(.contains mime-type %)
@@ -14,5 +25,6 @@
            "wordprocessingml"
            "presentationml"
            "application/rtf"
+           "application/epub+zip"
            "powerpoint"
            "opendocument.text"])))
