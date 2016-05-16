909f9053fb20572cdc955c09a8ea2143d537811b
Avi Flax <avi@aviflax.com>
2016-05-16 19:07:39 -0400

    WIP: Only index Box docs shared to entire company

--- src/omnia/services/box.clj
+++ src/omnia/services/box.clj
@@ -3,6 +3,8 @@
    because the Java SDK doesn’t support regular polling for events — only long-polling, which
    doesn’t fit our current needs.
 
+   ## Event Processing
+
    Note that there’s a cache for recently-processed-events due to this from the Box API docs:
 
    > Due to timing lag across our multiple datacenters, and our preference of low-latency and
@@ -10,7 +12,34 @@
    > call the /events API. You should use the event_id field in each returned event to check for
    > duplicate events and discard ones that you have already processed.
 
+   In practice, I’ve been seeing the same event returned over and over again for successive
+   requests. So this is more common than the above quote implies.
+
    https://box-content.readme.io/reference#events
+
+   ## Security
+
+   Box mainly relies on “collaborators” to share content, but we can’t rely on that because we have
+   no way to detect whether _everyone_ in a company has been invited to a folder as a collaborator.
+
+   So instead we rely on their “shared links” feature to determine whether a file or folder is
+   accessible to the entire company. Basically that’s just one of the supported settings on a
+   “shared link”. So when we first index, we list the root and look for folders in the root that
+   have shared links that are accessible to the entire company. Those folders and their entire
+   contents are considered eligible for indexing by Omnia, so that’s what gets indexed.
+
+   On incremental syncs, whenever we’re processing a change to a file, we check to ensure that the
+   file has one of the shared folders as one of its ancestors; if it does then we index it. If it
+   does not then we un-index it.
+
+   We should also be:
+
+   * looking for the creation of new shared-company-wide folders and adding them to the set
+   * looking for a folder revision wherein a folder is newly shared company-wide, and adding them
+     to the set
+   * looking for the deletion or un-sharing of folders and removing them from the set
+
+   BUT we might not get around to those features for awhile.
    "
   (:require [clojure.core.async :refer [chan dropping-buffer go <! >!!]]
             [clojure.core.cache :as cache]
@@ -24,12 +53,8 @@
             [pantomime.mime :refer [mime-type-of]]
             [pantomime.extract :refer [parse]]
             [clj-http.client :as client]
-            [taoensso.timbre :as timbre
-             :refer (log trace debug info warn error fatal report
-                         logf tracef debugf infof warnf errorf fatalf reportf
-                         spy get-env log-env)])
-  (:import [com.box.sdk BoxAPIConnection BoxUser BoxFolder BoxFile$Info BoxFolder$Info BoxFile BoxAPIException]
-           [java.io ByteArrayOutputStream]
+            [taoensso.timbre :as log])
+  (:import [com.box.sdk BoxAPIConnection BoxUser BoxFolder BoxFile$Info BoxFolder$Info BoxFile BoxAPIException BoxSharedLink$Access]
            [clojure.lang ExceptionInfo]))
 
 (def ^:private recently-processed-events (atom (cache/fifo-cache-factory {})))
@@ -55,7 +80,13 @@
   (get-auth-uris [_] auth)
   (get-user-account [_ access-token] (get-user access-token)))
 
-(defn ^:private should-index? [file-info] true)
+(defn ^:private should-index? [file-info]
+  true
+  ; the below is wrong — rather than checking that a file itself has a shared link with the required
+  ; access level, we should check whether a file is rooted in a root-level dir that has such a link.
+  ;(= (-> file-info .getSharedLink .getAccess)
+  ;   BoxSharedLink$Access/COMPANY)
+  )
 
 (defn ^:private build-path [file-info]
   (->> (.getPathCollection file-info)
@@ -129,7 +160,7 @@
 (defn ^:private get-events-resource
   "Returns a map containing the response and the potentially updated account."
   [account stream-position]
-  (info "Retrieving events for Box account" account "with stream position" stream-position)
+  (log/info "Retrieving events for Box account" account "with stream position" stream-position)
   (goget (:events urls) account {:query-params {"stream_position" stream-position
                                                 "limit"           10000}
                                  :as           :json}))
@@ -141,7 +172,7 @@
                                         "ITEM_TRASH" "ITEM_UNDELETE_VIA_TRASH"})
 
 (defn un-index-file [file-id account]
-  (info "un-indexing file" file-id)
+  (log/info "un-indexing file" file-id)
   (index/delete {:omnia-account-id (:id account)
                  :omnia-id         (omnia-id-for-file file-id)}))
 
@@ -183,10 +214,10 @@
     (let [{:keys [account response]} (get-events-resource account stream-position)
           {events               :entries
            next-stream-position :next_stream_position} (:body response)]
-      (debug "next-stream-position" next-stream-position "events" events)
+      (log/debug "next-stream-position" next-stream-position "events" events)
       (process-events events account)
       (when next-stream-position
-            (info "Setting Box sync-cursor to " next-stream-position)
+            (log/info "Setting Box sync-cursor to " next-stream-position)
             (db/update-account (:id account) :sync-cursor (str next-stream-position)))
       (when (seq events)
             (recur next-stream-position account)))))
@@ -205,7 +236,7 @@
       :skip-other)))
 
 (defn ^:private initial-index [account]
-  (infof "Starting initial index of account %s" account)
+  (log/infof "Starting initial index of account %s" account)
   (index-folder (BoxAPIConnection. (:access-token account))
                 "0"
                 account)
@@ -221,13 +252,11 @@
 (def ^:private sync-chan (chan (dropping-buffer 1)))
 
 (defn ^:private start []
-  (go
-    (loop []
-      (when-some [account (<! sync-chan)]
-        (try
-          (synchronize! account)
-          (catch Exception e (error e)))
-        (recur)))))
+  (go (loop []
+        (when-some [account (<! sync-chan)]
+          (try (synchronize! account)
+               (catch Exception e (log/error e)))
+          (recur)))))
 
 (defrecord Account [id user service access-token sync-cursor]
   accounts/Account
